80ffc3852b190fb5256a3beab1f3229fca4b8b27
Natalie Wolfe <natalie@lifewanted.com>
2016-09-17 16:31:01 -0700

    Added gulp build

--- assets/scripts/editor/components/editor.js
+++ assets/scripts/editor/components/editor.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var debounce = require('throttle-debounce/debounce');
 var React = require('react');
 var ReactAce = require('react-ace').default;
 
@@ -7,13 +8,33 @@ require('brace/mode/c_cpp');
 require('brace/theme/ambiance');
 
 
+const CHANGE_DEBOUNCE_MS = 500;
+
+var onEditorChange = debounce(CHANGE_DEBOUNCE_MS, (code) => {
+});
+
+var ResultsBox = React.createClass({
+    getInitialState: () => ({runResults: '', compileResults: ''}),
+    render: function() {
+        return (
+            <div className="resultsBox">
+                <div>{this.state.compileResults}</div>
+                <hr />
+                <div>{this.state.runResults}</div>
+            </div>
+        );
+    }
+});
+
 var EditorBox = React.createClass({
     render: () => (
-        <div className="editorBox">
-            <form method="post" action="/execute">
-                <div><ReactAce mode="c_cpp" theme="ambiance" name="code" /></div>
-                <div><button type="submit">Execute!</button></div>
-            </form>
+        <div className="editorBox panelist">
+            <div className="panel-75">
+                <ReactAce mode="c_cpp" theme="ambiance" width="100%" height="100%" onChange={onEditorChange} />
+            </div>
+            <div className="panel-25">
+                <ResultsBox />
+            </div>
         </div>
     )
 });
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+
+gulp.task('scripts', () => {
+    gulp.src('./assets/scripts/editor/index.js')
+        .pipe(browserify({
+            transform: ['babelify'],
+            insertGlobals: true
+        }))
+        .pipe(concat('editor.js'))
+        .pipe(gulp.dest('./static/scripts/'));
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./assets/styles/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./static/styles'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./assets/scripts/**/*.js', ['scripts']);
+    gulp.watch('./assets/styles/**/*.less', ['styles']);
+});
+
+gulp.task('default', ['scripts', 'styles']);
